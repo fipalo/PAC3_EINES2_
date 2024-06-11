c19fdb4627b4464f97944dfbd152dc9c2c6e87ac
fipalo <fionaparis99@gmail.com>
2024-06-11 23:40:51 +0200

    1

---
+++ uoc-boilerplate-master/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/assets/styles/tailwind.css.{html,js}'],
+  darkMode: false, // o 'media' o 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
